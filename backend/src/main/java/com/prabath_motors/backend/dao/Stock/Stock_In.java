ebbf47b1adc37e5505dccac255e99c8914af4b15
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-03-21 09:27:21 +0530

    Stock_In crud operations

---
+++ backend/src/main/java/com/prabath_motors/backend/dao/Stock/Stock_In.java
@@ -0,0 +1,31 @@
+package com.prabath_motors.backend.dao.Stock;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Stock_In {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int stockInID;
+    private int itemID;
+    private int supplierID;
+    private int qtyAdded;
+    private double unitPrice;
+    private double totalCost;
+    private LocalDate dateAdded;
+}
---
+++ backend/src/main/java/com/prabath_motors/backend/service/stockService/Stock_InServiceImpl.java
@@ -0,0 +1,62 @@
+package com.prabath_motors.backend.service.stockService;
+
+import com.prabath_motors.backend.dao.Stock.Stock_In;
+import com.prabath_motors.backend.repository.Stock_InRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class Stock_InServiceImpl implements Stock_InService {
+    private final Stock_InRepository stockInRepository;
+
+    @Autowired
+    public Stock_InServiceImpl(Stock_InRepository stockInRepository) {this.stockInRepository = stockInRepository;}
+
+    @Override
+    public List<Stock_In> getAllStocks_In(){
+        List<Stock_In> stocks = stockInRepository.findAll();
+        if (stocks.isEmpty()) {
+            throw new RuntimeException("No stocks found");
+        }
+        return stocks;
+    }
+
+    @Override
+    public Stock_In getStockById(Integer stockInID){
+        return stockInRepository.getReferenceById(stockInID);
+    }
+
+    @Override
+    public Stock_In SaveStockIn(Stock_In stock){
+        return stockInRepository.save(stock);
+    }
+
+    @Override
+    public Stock_In UpdateStockInDetails(Integer id, Stock_In stock){
+        Optional<Stock_In> existingStock = stockInRepository.findById(id);
+
+        if(existingStock.isPresent()){
+            Stock_In existingStockIn = existingStock.get();
+
+            //Update only the necessary fields
+            existingStockIn.setItemID(stock.getItemID());
+            existingStockIn.setSupplierID(stock.getSupplierID());
+            existingStockIn.setQtyAdded(stock.getQtyAdded());
+            existingStockIn.setUnitPrice(stock.getUnitPrice());
+            existingStockIn.setTotalCost(stock.getTotalCost());
+            existingStockIn.setDateAdded(stock.getDateAdded());
+
+            return stockInRepository.save(existingStockIn);
+        }else {
+            throw new RuntimeException("Stock not found with ID : " + id);
+        }
+    }
+
+    @Override
+    public void DeleteStockById(Integer id){
+        stockInRepository.deleteById(id);
+    }
+}
