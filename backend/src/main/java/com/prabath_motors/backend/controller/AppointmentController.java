789106ec1a35f432036981ed4195ff28778131bc
dilumhapugoda <dilumhapugoda@gmail.com>
2025-03-22 15:24:00 +0530

    add cross origins

--- backend/src/main/java/com/prabath_motors/backend/controller/AppointmentController.java
+++ backend/src/main/java/com/prabath_motors/backend/controller/AppointmentController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/appointment")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AppointmentController {
 
     @Autowired
