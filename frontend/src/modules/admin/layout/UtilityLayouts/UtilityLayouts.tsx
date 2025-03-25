1189324b4bbde940218579cf3609b84d9002948f
Hashini Divyanjali <hashini190071@gmail.com>
2025-03-25 09:52:21 +0530

    trial-run

---
+++ frontend/src/modules/admin/layout/UtilityLayouts/UtilityLayouts.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import UtilityNav from "./UtilityNavigation";
+
+interface StockLayoutProps {
+  children: React.ReactNode;
+}
+
+const StocksLayout: React.FC<StockLayoutProps> = ({ children }) => {
+  return (
+    <div className="max-w-full pattern-dots pattern-green-300 dark:pattern-green-950 pattern-bg-white dark:pattern-bg-black pattern-size-2 pattern-opacity-100">
+      <UtilityNav />
+        <div className="min-h-screen">
+            {children}
+        </div>
+    </div>
+  );
+};
+
+export default StocksLayout;
