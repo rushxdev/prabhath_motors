fbb68c6b8f597ab568c4392afbe4a6d6b03f9e2f
dilumhapugoda <dilumhapugoda@gmail.com>
2025-03-25 10:34:35 +0530

    job-forgotton where the hell I stopped

--- backend/src/main/java/com/prabath_motors/backend/dao/Job.java
+++ backend/src/main/java/com/prabath_motors/backend/dao/Job.java
@@ -1,4 +1,38 @@
 package com.prabath_motors.backend.dao;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "jobs")
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jobId;
+
+    @Column(nullable = false)
+    private String vehicleRegistrationNumber; //auto fetch from appointment
+
+    @Column(nullable = false)
+    private String serviceSection;
+
+    @Column(nullable = false)
+    private Long employeeId;
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
+    private List<JobTask> tasks; //Multiple tasks assigned to a job
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
+    private List<JobSparePart> spareParts;
+
+    @Column(nullable = false)
+    private String status; //Ongoing or Done
 }
