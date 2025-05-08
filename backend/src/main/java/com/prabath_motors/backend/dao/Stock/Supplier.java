b506058df936ac089012b6c51dfd14d18f030c71
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-05-08 09:22:35 +0530

    Backend validation done - stocks

--- backend/src/main/java/com/prabath_motors/backend/dao/Stock/Supplier.java
+++ backend/src/main/java/com/prabath_motors/backend/dao/Stock/Supplier.java
@@ -1,10 +1,8 @@
 package com.prabath_motors.backend.dao.Stock;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +16,15 @@ public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int supplierId;
+    
+    @NotBlank(message = "Supplier name is required")
+    @Size(min = 2, max = 100, message = "Supplier name must be between 2 and 100 characters")
     private String supplierName;
+    
+    @NotBlank(message = "Contact person name is required")
+    @Size(min = 2, max = 100, message = "Contact person name must be between 2 and 100 characters")
     private String contactPerson;
+    
+    @Digits(integer = 10, fraction = 0, message = "Phone number must be numeric and have at most 10 digits")
     private int phoneNumber;
 }
